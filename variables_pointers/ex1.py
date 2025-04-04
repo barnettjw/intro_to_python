1bf976b7294c5bc604cad5e1384278e1808ace79
James Barnett <barnettjw@gmail.com>
2025-04-04 13:55:15 -0400

    Chapter exercises - variables_pointers

---
+++ variables_pointers/ex1.py
@@ -0,0 +1,6 @@
+# my_object1 == my_object2
+# The == operator checks if object1 has the same value as object2 or
+# in the case collections have the same elements
+
+# my_object1 is my_object2
+# The is operator checks if object1 and object2 both point to the same location in memory
