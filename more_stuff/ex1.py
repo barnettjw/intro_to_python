ca68422599fd24661a10fb1c27c3792b0312b78e
James Barnett <barnettjw@gmail.com>
2025-04-04 13:55:42 -0400

    Chapter exercises - more_stuff

---
+++ more_stuff/ex1.py
@@ -0,0 +1,17 @@
+def do_something(dictionary):
+    return sorted(dictionary.keys())[1].upper()
+
+my_dict = {
+    'Karl':     108,
+    'Clare':    175,
+    'Karis':    140,
+    'Trevor':   180,
+    'Antonina': 132,
+    'Chris':    101,
+}
+
+print(do_something(my_dict))
+
+# Sort a view of dictionary keys
+# access the value at the 2nd index (of the sorted keys) and make it uppercase
+# returns CHRIS
